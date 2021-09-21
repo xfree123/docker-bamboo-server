423c5417c12fe64828fab415451f072af3a20fd7
Steve Smith <ssmith@atlassian.com>
2021-09-21 11:57:18 +1000

    BAMBK8S-8: Add start of Pipelines port.

---
+++ pipelines-generator.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import os
+import jinja2 as j2
+
+TEMPLATE_FILE = 'bitbucket-pipelines.yml.j2'
+images = {
+
+    'Bamboo': {
+        11: {
+            'mac_key': 'bamboo',
+            'start_version': '8',
+            'end_version': '9',
+            'default_release': True,
+            'base_image': 'adoptopenjdk/openjdk:11',
+            'tag_suffixes': ['jdk11', 'ubuntu'],
+            'dockerfile': 'Dockerfile',
+            'docker_repos': ['atlassian/bamboo', 'atlassian/bamboo-server'],
+        }
+    }
+}
+
+
+def main():
+    jenv = j2.Environment(
+        loader=j2.FileSystemLoader('.'),
+        lstrip_blocks=True,
+        trim_blocks=True)
+    template = jenv.get_template(TEMPLATE_FILE)
+    generated_output = template.render(images=images, batches=15)
+
+    print(generated_output)
+
+if __name__ == '__main__':
+    main()
