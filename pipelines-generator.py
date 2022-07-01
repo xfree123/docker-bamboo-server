00194f389ab0a8163877f711ef5c0a32ce125131
nghazalibeiklar <nghazalibeiklar@atlassian.com>
2022-07-01 12:13:14 +1000

    CLIP-1586: Removed upper limit version.

--- pipelines-generator.py
+++ pipelines-generator.py
@@ -9,7 +9,6 @@ images = {
         11: {
             'mac_key': 'bamboo',
             'start_version': '7.1',
-            'end_version': '9',
             'default_release': True,
             'base_image': 'eclipse-temurin:11',
             'tag_suffixes': ['jdk11', 'ubuntu'],
