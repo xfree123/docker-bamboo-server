bc8dc584090eb3acab69853802e5e6df77662a6a
nghazalibeiklar <nghazalibeiklar@atlassian.com>
2022-07-05 16:06:13 +1000

    CLIP-1586: added upper version optional. Also included bamboo 7-8 build using jira 8

--- pipelines-generator.py
+++ pipelines-generator.py
@@ -8,10 +8,20 @@ images = {
     'Bamboo': {
         11: {
             'mac_key': 'bamboo',
-            'start_version': '7.1',
+            'start_version': '8',
             'default_release': True,
             'base_image': 'eclipse-temurin:11',
             'tag_suffixes': ['jdk11', 'ubuntu'],
+            'dockerfile': 'Dockerfile',
+            'docker_repos': ['atlassian/bamboo', 'atlassian/bamboo-server'],
+        },
+        8: {
+            'mac_key': 'bamboo',
+            'start_version': '7.1',
+            'end_version': '8',
+            'default_release': True,
+            'base_image': 'eclipse-temurin:8',
+            'tag_suffixes': ['jdk8', 'ubuntu'],
             'dockerfile': 'Dockerfile',
             'docker_repos': ['atlassian/bamboo', 'atlassian/bamboo-server'],
         }
